b476c197e06288988b9a570c10a863dff1be4336
adzuki1 <mneto.fis@gmail.com>
2025-07-24 19:24:52 -0300

    update main

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,17 @@
 // evocando lib padrão input/output
 use std::io;
+
 use rand::Rng;
+use std::cmp::Ordering;
 
 fn main() {
     println!("\n### GUESSING GAME ###\n");
+
+    let num = rand::thread_rng()
+        .gen_range(1..=100);
+
     println!("Entre seu palpite: ");
-    
+
     let mut guess = String::new(); // cria nova string vazia UTF-8 expansível
 
     // a seguinte lógica pode ser escrita de duas maneiras:
@@ -15,5 +21,11 @@ fn main() {
         .expect("Failed to read line");
 
     println!("Palpite: {guess}");
+
+    match guess.cmp(&num){
+        Ordering::Less => println!("Muito pequeno!"),
+        Ordering::Greater => println!("Muito grande!"),
+        Ordering::Equal => println!("na medida certa!"),
+    }
 }
 
